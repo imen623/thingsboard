3d5963577e2116d2e56d23b8e1495cdac67bc353
imen623 <hachicha.imane@gmail.com>
2025-07-10 10:56:13 +0100

    rm dashboard

--- ngx-admin/src/app/pages/pages.module.ts
+++ ngx-admin/src/app/pages/pages.module.ts
@@ -3,7 +3,6 @@ import { NbMenuModule } from "@nebular/theme";
 
 import { ThemeModule } from "../@theme/theme.module";
 import { PagesComponent } from "./pages.component";
-import { DashboardModule } from "./dashboard/dashboard.module";
 import { ECommerceModule } from "./e-commerce/e-commerce.module";
 import { PagesRoutingModule } from "./pages-routing.module";
 import { MiscellaneousModule } from "./miscellaneous/miscellaneous.module";
@@ -16,7 +15,7 @@ import { FormsModule } from "@angular/forms";
     PagesRoutingModule,
     ThemeModule,
     NbMenuModule,
-    DashboardModule,
+
     ECommerceModule,
     MiscellaneousModule,
   ],
